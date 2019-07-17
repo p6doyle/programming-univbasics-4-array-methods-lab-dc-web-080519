d1a9ab90f0a463d19850b149d687d72161f85d0d
Peyton Doyle <p6doyle@gmail.com>
2019-07-17 09:40:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,12 +3,10 @@ array.include?(element)
 end
 
 def using_sort(array)
-array = ["Jim", "wow", "I"]
 array.sort
 end
 
 def using_reverse(array)
-array = ["Jim", "wow", "arrays!"]
 array.reverse
 end
 
